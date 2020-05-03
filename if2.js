4e82280c9711027118d6d79846276edb29432125
Anupam Pandey <pandey.anupam873@gmail.com>
2020-05-03 17:35:49 +0530

    html file

---
+++ if2.js
@@ -0,0 +1,9 @@
+var num=Number(prompt("Enter a number"));
+if(num%2===0){
+    console.log("Given number is Even");
+    alert("Number is Even");
+}
+else{
+    console.log("Number is odd");
+    alert("Number is odd");
+}
